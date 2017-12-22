2480f9e8fec2eae82884c9c9f0302a79b86f6eed
Sandro Koch <layornos@gmail.com>
2017-12-22 19:37:25 +0000

    gr

--- jobs/kvv_auerstr.rb
+++ jobs/kvv_auerstr.rb
@@ -1,9 +1,9 @@
 require_relative 'kvv'
 
 SCHEDULER.every '20s', :first_in => 0 do
-  auerstr_id = "de:8212:11"
+  groetz_id = "de:8212:1521"
 
-  data_auerstr = get_update_item(auerstr_id)
+  data_groetz = get_update_item(groetz_id)
 
-  send_event('departures_auerstr', data_auerstr)
+  send_event('departures_groetz', data_groetz)
 end
